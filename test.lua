b6e4c85cc20e376c03a003f1fc17e7a37baa0306
KinbunLuk <44672732+KinbunLuk@users.noreply.github.com>
2019-05-29 16:58:27 +0800

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,22 @@
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber('4,692,750,812,894,068,736',gg.TYPE_QWORD, false, gg.SIGN_EQUAL,0, -1)
+local n = gg.getResultCount()
+local t = gg.getResults(n)
+local isOpen = 0
+for i=1,#t do
+    local check = gg.getValues({[1]={address = t[i].address-0xc,flags = gg.TYPE_DWORD}})
+	local check2 = gg.getValues({[1]={address = t[i].address-0x224,flags = gg.TYPE_FLOAT}})
+	local check3 = gg.getValues({[1]={address = t[i].address-0x200,flags = gg.TYPE_DWORD}})
+	local check4 = gg.getValues({[1]={address = t[i].address-0x1f0,flags = gg.TYPE_DWORD}})
+	if(check2[1].value >10000 and check2[1].value < 150000 and check[1].value == 1116471296 and check3[1].value == check4[1].value) then
+    gg.setValues({[1]={address=t[i].address-0x224,flags=gg.TYPE_FLOAT,value=500000}})   
+	isOpen = 1
+	end
+end
+if(isOpen == 1) then
+gg.toast('Bullet Speed OK')
+else
+gg.toast('Value Not Found')
+end
+gg.clearResults()
