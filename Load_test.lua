3af10a85d4b6fd834f4115246b7a3adaef0b0e63
KinbunLuk <44672732+KinbunLuk@users.noreply.github.com>
2019-05-29 16:57:44 +0800

    Delete Load_test.lua

--- Load_test.lua
+++
@@ -1,6 +0,0 @@
-local file = gg.makeRequest("https://raw.githubusercontent.com/KinbunLuk/public_script/master/main.lua")
-  local load = load(file.content)
-  if load == nil then
-    os.exit()
-  end
-  load()
