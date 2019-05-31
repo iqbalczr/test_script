b1cb3e190d6af492f75282aae34c834a93436a87
KinbunLuk <44672732+KinbunLuk@users.noreply.github.com>
2019-06-01 00:28:16 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,10 +5,12 @@ local n = gg.getResultCount()
 local t = gg.getResults(n)
 local isOpen = 0
 for i=1,#t do
+	local check = gg.getValues({[1]={address = t[i].address-0xc,flags = gg.TYPE_DWORD}})
 	local check2 = gg.getValues({[1]={address = t[i].address-0x224,flags = gg.TYPE_FLOAT}})
 	local check3 = gg.getValues({[1]={address = t[i].address-0x200,flags = gg.TYPE_DWORD}})
 	local check4 = gg.getValues({[1]={address = t[i].address-0x1f0,flags = gg.TYPE_DWORD}})
-	if(check2[1].value >10000 and check2[1].value < 150000 and check3[1].value == check4[1].value) then
+	local check5 = gg.getValues({[1]={address = t[i].address-0x21C,flags = gg.TYPE_DWORD}})
+	if(check2[1].value >10000 and check2[1].value < 150000 and check[1].value == 1116471296 and check3[1].value == check4[1].value and check5[1].value ~= 0) then
     gg.addListItems({[1]={address=t[i].address-0x224,flags=gg.TYPE_FLOAT,value=500000}})   
 	isOpen = 1
 	end
