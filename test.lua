dc7b5c7aefc83887975bb5caeecebc538af5cc6a
KinbunLuk <44672732+KinbunLuk@users.noreply.github.com>
2019-06-06 23:39:43 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,7 +6,7 @@ local t = gg.getResults(n)
 local isOpen = 0
 for i=1,#t do
 	local check = gg.getValues({[1]={address = t[i].address+0x4,flags = gg.TYPE_FLOAT}})
-	if(check[1].value >-1e28 and check[1].value < -9e28) then
+	if(check[1].value < -1e28 and check[1].value > -9e28) then
     gg.addListItems({[1]={address=t[i].address-0x224,flags=gg.TYPE_FLOAT,value=500000}})   
 	isOpen = 1
 	end
