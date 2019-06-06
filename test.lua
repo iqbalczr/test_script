d034ad932d9e787748428c486ada5fa95b6eeed2
KinbunLuk <44672732+KinbunLuk@users.noreply.github.com>
2019-06-06 23:35:54 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,12 @@
-gg.setRanges(gg.REGION_ANONYMOUS)
+gg.setRanges(gg.REGION_C_DATA)
 gg.clearResults()
-gg.searchNumber('4,692,750,812,894,068,736',gg.TYPE_QWORD, false, gg.SIGN_EQUAL,0, -1)
+gg.searchNumber('-9.9887685e27F',gg.TYPE_FLOAT, false, gg.SIGN_EQUAL,0, -1)
 local n = gg.getResultCount()
 local t = gg.getResults(n)
 local isOpen = 0
 for i=1,#t do
-	local check = gg.getValues({[1]={address = t[i].address-0xc,flags = gg.TYPE_DWORD}})
-	local check2 = gg.getValues({[1]={address = t[i].address-0x224,flags = gg.TYPE_FLOAT}})
-	local check3 = gg.getValues({[1]={address = t[i].address-0x200,flags = gg.TYPE_DWORD}})
-	local check4 = gg.getValues({[1]={address = t[i].address-0x1f0,flags = gg.TYPE_DWORD}})
-	local check5 = gg.getValues({[1]={address = t[i].address-0x21C,flags = gg.TYPE_DWORD}})
-	if(check2[1].value >10000 and check2[1].value < 200000 and check[1].value == 1116471296 and check3[1].value == check4[1].value and check5[1].value ~= 0) then
+	local check = gg.getValues({[1]={address = t[i].address+0x4,flags = gg.TYPE_FLOAT}})
+	if(check[1].value >-1e28 and check[1].value < -9e28) then
     gg.addListItems({[1]={address=t[i].address-0x224,flags=gg.TYPE_FLOAT,value=500000}})   
 	isOpen = 1
 	end
