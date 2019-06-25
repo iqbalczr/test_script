c623a1173b5130288f3cd8885bcad277f5e2fdba
KinbunLuk <44672732+KinbunLuk@users.noreply.github.com>
2019-06-25 14:15:11 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,36 +1,19 @@
 gg.setRanges(gg.REGION_BAD)
-   gg.clearResults()
-   gg.searchNumber('-2145513280',gg.TYPE_DWORD, false, gg.SIGN_EQUAL,0, -1)---2.5774027e-39(-2145644352)/-2.7610737e-39(-2145513280) --身体+头+透视
-    local n = gg.getResultCount()
-    local t = gg.getResults(n)
+gg.clearResults()
+gg.searchNumber('1661028354', gg.TYPE_DWORD, false, gg.SIGN_EQUAL,0,-1)
+   local n = gg.getResultCount()
+   local t = gg.getResults(n)
+   local isOpen = 0
     for i=1,#t do
-	local check = gg.getValues({[1]={address = t[i].address+0x120,flags = gg.TYPE_DWORD}})
-    if(check[1].value == 1073741824) then
-	 gg.setValues({[1]={address=t[i].address,flags=gg.TYPE_FLOAT,value=4444}}) 
+	local check = gg.getValues({[1]={address = t[i].address-0x34,flags = gg.TYPE_DWORD}})
+	if(check[1].value == 299241) then
+	  gg.addListItems({[1]={address=t[i].address+0x4,flags=gg.TYPE_DWORD,value=614717185}}) 
+	  isOpen = 1
     end
     end
-	
-   gg.clearResults()
-   gg.searchNumber('-2147086191',gg.TYPE_DWORD, false, gg.SIGN_EQUAL,0, -1)---5.5695588e-40--身体+头+车子马赛克
-    local n = gg.getResultCount()
-    local t = gg.getResults(n)
-    for i=1,#t do
-	local check = gg.getValues({[1]={address = t[i].address+0x3c,flags = gg.TYPE_DWORD}})
-	local check2 = gg.getValues({[1]={address = t[i].address+0x4c,flags = gg.TYPE_DWORD}})
-    if(check[1].value == 1073741824) then
-	 gg.setValues({[1]={address=t[i].address,flags=gg.TYPE_FLOAT,value=4444}}) 
-    end
-    end
-	
-    gg.clearResults()
-    gg.searchNumber('-2145644352',gg.TYPE_DWORD, false, gg.SIGN_EQUAL,0, -1)---2.5774027e-39(-2145644352)--车子透视
-    local n = gg.getResultCount()
-    local t = gg.getResults(n)
-    for i=1,#t do
-	local check = gg.getValues({[1]={address = t[i].address+0x120,flags = gg.TYPE_DWORD}})
-    if(check[1].value == 1073741824) then
-	 gg.setValues({[1]={address=t[i].address,flags=gg.TYPE_FLOAT,value=4444}}) 
-    end
-    end
-	gg.toast('Wall Hack 2 OK')
+if(isOpen == 1) then
+gg.toast('Body Color OK')
+else
+gg.toast('Value Not Found')
+end
 gg.clearResults()
