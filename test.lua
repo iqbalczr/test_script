2b3ddc2a95a6920a621acd18b74f63db250590b9
KinbunLuk <44672732+KinbunLuk@users.noreply.github.com>
2019-06-28 23:06:08 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,19 +1,33 @@
-gg.setRanges(gg.REGION_BAD)
-gg.clearResults()
-gg.searchNumber('1661028354', gg.TYPE_DWORD, false, gg.SIGN_EQUAL,0,-1)
-   local n = gg.getResultCount()
-   local t = gg.getResults(n)
-   local isOpen = 0
-    for i=1,#t do
-	local check = gg.getValues({[1]={address = t[i].address-0x34,flags = gg.TYPE_DWORD}})
-	if(check[1].value == 299241) then
-	  gg.addListItems({[1]={address=t[i].address+0x4,flags=gg.TYPE_DWORD,value=614717185}}) 
-	  isOpen = 1
+    gg.setRanges(gg.REGION_BAD)
+	gg.clearResults()
+    gg.searchNumber('537149445', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+	n = gg.getResultCount()
+	local t = gg.getResults(n)
+	for i=1,#t do
+	local check = gg.getValues({[1]={address = t[i].address-0x4,flags = gg.TYPE_DWORD}})
+	local check2 = gg.getValues({[1]={address = t[i].address-0xC,flags = gg.TYPE_DWORD}})
+	local check3 = gg.getValues({[1]={address = t[i].address-0x4c,flags = gg.TYPE_DWORD}})
+	local check4 = gg.getValues({[1]={address = t[i].address+0x84,flags = gg.TYPE_DWORD}})
+    if(check[1].value == 9 and check2[1].value == 6 and check3[1].value == 4140 and check4[1].value == 1073741824) then
+	gg.addListItems({[1]={address=t[i].address+0x84,flags=gg.TYPE_FLOAT,value=9999}}) 
     end
     end
-if(isOpen == 1) then
-gg.toast('Body Color OK')
-else
-gg.toast('Value Not Found')
-end
-gg.clearResults()
+	gg.toast('No Flick 50%')
+	gg.clearResults()
+	
+    gg.searchNumber('1087373317', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+	n = gg.getResultCount()
+	local t = gg.getResults(n)
+	for i=1,#t do
+	local check = gg.getValues({[1]={address = t[i].address-0xc,flags = gg.TYPE_DWORD}}) 
+	local check2 = gg.getValues({[1]={address = t[i].address+0x20,flags = gg.TYPE_DWORD}}) 
+	local check3 = gg.getValues({[1]={address = t[i].address+0x8c,flags = gg.TYPE_DWORD}}) 
+	local check4 = gg.getValues({[1]={address = t[i].address+0x90,flags = gg.TYPE_DWORD}})  
+	local check5 = gg.getValues({[1]={address = t[i].address+0x94,flags = gg.TYPE_DWORD}})  
+	local check6 = gg.getValues({[1]={address = t[i].address+0x98,flags = gg.TYPE_DWORD}})  
+    if(check[1].value == 4140 and check2[1].value == 50331648 and check3[1].value == 1132396544 and check4[1].value == 1065353216 and check5[1].value == 1073741824 and check6[1].value == 1084227584) then
+	gg.addListItems({[1]={address=t[i].address+0x94,flags=gg.TYPE_FLOAT,value=9999}}) 
+    end
+    end
+	gg.toast('No Flick 100%')
+	gg.clearResults()
